63dbff0ff249bd809808e5388cc2f9d95548260e
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-18 13:17:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.OData.SampleService.Models.TripPin;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Demo_ODtata_Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // from c# 7.1 you can use async Main instead
+            ListPeople().Wait();
+        }
+
+        static async Task ListPeople()
+        {
+            var serviceRoot = "https://services.odata.org/V4/TripPinServiceRW/";
+            var context = new DefaultContainer(new Uri(serviceRoot));
+
+            IEnumerable<Person> people = await context.People.ExecuteAsync();
+            foreach (var person in people)
+            {
+                Console.WriteLine("{0} {1}", person.FirstName, person.LastName);
+            }
+        }
+    }
+}
